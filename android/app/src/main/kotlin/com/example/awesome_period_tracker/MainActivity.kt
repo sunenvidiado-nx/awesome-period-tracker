e3aa5cf0b4626385b921407801a3bb688d8be362
Sun Adriann Envidiado <sunadriann31@gmail.com>
2025-01-02 11:51:37 +0800

    fix: 🐛 Fix android build issues

--- android/app/src/main/kotlin/com/example/awesome_period_tracker/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sunadriann.awesome_period_tracker
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity()
---
+++ android/app/src/main/kotlin/com/sunadriann/awesome_period_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sunadriann.awesome_period_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
